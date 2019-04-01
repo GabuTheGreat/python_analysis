d3a836d005b0b67e63f427dd35abaf78841e2224
Agnes <kamenjuagnes@gmail.com>
2019-04-02 00:22:01 +0300

    Adding link to dashboard

--- Dashboard codes.py
+++ Dashboard codes.py
@@ -4,6 +4,10 @@ Created on Fri Mar 29 12:06:24 2019
 
 @author: X260
 """
+#importing pandas
+import pandas as pd
+
+all_tb = pd.read_csv ('~/Downloads/TB_burden_countries_2019-01-11.csv', encoding = 'utf-8')
 
 
 
